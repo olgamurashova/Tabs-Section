8d17555b792c3c7b56dc87557e2dc8da59a6e9c8
Olga Murashova <om.dreyfus@gmail.com>
2022-10-05 00:09:28 -0400

    main.js

---
+++ main.js
@@ -0,0 +1,26 @@
+const tabs = document.querySelector(".tabs");
+
+const btns = document.querySelectorAll(".button");
+
+const content = document.querySelectorAll(".content");
+
+
+tabs.addEventListener("click",(event)=>{
+    
+    const id = event.target.dataset.id;
+    if(id) {
+        btns.forEach((btn)=> {
+            btn.classList.remove("live");
+
+        });
+        event.target.classList.add("live");
+        content.forEach((content)=>{
+            content.classList.remove("live");
+
+        });
+        const element = document.getElementById(id);
+        element.classList.add("live");
+
+
+    }
+});
